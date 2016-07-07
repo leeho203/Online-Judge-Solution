57c15d1549b1cbff8b15d52d9d156a6581a12d28
leeho203 <leeho203@naver.com>
2016-07-08 03:15:28 +0900

    Update 1010 다리놓기.java

--- 1010 다리놓기.java
+++ 1010 다리놓기.java
@@ -14,7 +14,7 @@ public class Main {
 		System.out.print(result);
 	}
   
-  // 이 문제를 단순히 생각해보면 강 동쪽 사이트가 강 서쪽 사이트를 순서에 상관없이 선택하는 것과 같음
+  	// 이 문제를 단순히 생각해보면 강 동쪽 사이트가 강 서쪽 사이트를 순서에 상관없이 선택하는 것과 같음
 	static int combination(int n, int k) {
 		if (k == n)
 			return 1;
