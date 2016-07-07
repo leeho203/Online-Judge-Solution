d43e2aacf3b2b991d9ab56e47c53a8b27f69053b
leeho203 <leeho203@naver.com>
2016-07-08 03:15:14 +0900

    Create 1010 다리놓기.java

---
+++ 1010 다리놓기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String result = "";
+		int t = sc.nextInt();
+		for (int i = 1; i <= t; i++) {
+			int k = sc.nextInt();
+			int n = sc.nextInt();
+			result += combination(n, k) + "\n";
+		}
+
+		System.out.print(result);
+	}
+  
+  // 이 문제를 단순히 생각해보면 강 동쪽 사이트가 강 서쪽 사이트를 순서에 상관없이 선택하는 것과 같음
+	static int combination(int n, int k) {
+		if (k == n)
+			return 1;
+		if (k == 1)
+			return n;
+		return combination(n - 1, k) + combination(n - 1, k - 1);
+	}
+}
