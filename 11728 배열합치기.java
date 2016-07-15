ebb66b6f916e7bdf0a2c7feaa8a9578fc66ea263
Lee, Ho <leeho203@naver.com>
2016-07-16 00:56:20 +0900

    Update 11728 배열합치기.java

--- 11728 배열합치기.java
+++ 11728 배열합치기.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main {
 	public static void main(String args[]) throws IOException {
 	
-	  // input과 output이 최대 2백만개이므로 BufferedReader와 BufferedWriter를 써야함
+	  	// input과 output이 최대 2백만개이므로 BufferedReader와 BufferedWriter를 써야함
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
